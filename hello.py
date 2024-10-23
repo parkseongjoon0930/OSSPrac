d64b51cf13b542869799961da525dd2ccae72ebd
parkseongjoon0930 <psj5947@dgu.ac.kr>
2024-10-24 00:45:42 +0900

    practice

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
-name = input("이름을 입력하세요: ")
-student_id = input("학번을 입력하세요: ")
-student_mj = input("학과를 입력하세요: ")
-student_sc = input("학교를 입력하세요: ")
-student_gr = input("학년을 입력하세요: ")
+name = input("이름을 입력")
+student_id = input("학번을 입력")
+student_mj = input("학과를 입력")
+student_sc = input("학교를 입력")
+student_gr = input("학년을 입력")
 
 
-
+#연습
 
 print()
 print("<출력>")
